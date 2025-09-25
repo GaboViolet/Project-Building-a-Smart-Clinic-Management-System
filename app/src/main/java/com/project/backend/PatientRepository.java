2f03b365455a5a7dad49e750295ad2183aa6a155
GaboViolet <gabokan02564@gmail.com>
2025-09-25 13:25:43 +0700

    Delete app/src/main/java/com/project/backend/PatientRepository.java

--- app/src/main/java/com/project/backend/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.backend.repo;
-
-import com.project.backend.models.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
